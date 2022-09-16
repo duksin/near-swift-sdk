46a7295fd7b6dbd453bc299400d0ed1815243e0e
Patrik Duksin <patrikduksin@icloud.com>
2022-09-16 19:00:41 +0800

    :ambulance: fix: crash while decode network responses

--- Sources/NearSwift/Utils/Web.swift
+++ Sources/NearSwift/Utils/Web.swift
@@ -9,52 +9,52 @@
 import Foundation
 
 public struct ConnectionInfo {
-  let url: URL
-  let user: String? = nil
-  let password: String? = nil
-  let allowInsecure: Bool? = nil
-  let timeout: TimeInterval? = nil
-  let headers: [String: Any]? = nil
+    let url: URL
+    let user: String? = nil
+    let password: String? = nil
+    let allowInsecure: Bool? = nil
+    let timeout: TimeInterval? = nil
+    let headers: [String: Any]? = nil
 }
 
 enum HTTPError: Error {
-  case unknown
-  case error(status: Int, message: String?)
+    case unknown
+    case error(status: Int, message: String?)
 }
 
 private func fetch(url: URL, params: [String: Any]?) async throws-> Any {
-  let session = URLSession.shared
-  var request = URLRequest(url: url)
-  request.httpMethod = params.flatMap {_ in "POST"} ?? "GET"
-  request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-  request.httpBody = params.flatMap { try? $0.toData() }
-  return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<Any, Error>) in
-    let task = session.dataTask(with: request) { data, response, error in
-      if let error = error { return continuation.resume(throwing: error) }
-      let result = data.flatMap {try? $0.toDictionary()}
-      if let json = result?["result"] {
-        continuation.resume(returning: json)
-      } else if let httpResponse = response as? HTTPURLResponse {
-        let json = try! JSONSerialization.data(withJSONObject: result, options: [])
-        debugPrint("=====================")
-        print(String(decoding: json, as: UTF8.self))
-        debugPrint("=====================")
-        let error = HTTPError.error(status: httpResponse.statusCode,
-                                    message: data.flatMap({ String(data: $0, encoding: .utf8) }))
-        continuation.resume(throwing: error)
-      } else {
-        continuation.resume(throwing: HTTPError.unknown)
-      }
-    }
-    task.resume()
-  })
+    let session = URLSession.shared
+    var request = URLRequest(url: url)
+    request.httpMethod = params.flatMap {_ in "POST"} ?? "GET"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = params.flatMap { try? $0.toData() }
+    return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<Any, Error>) in
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error { return continuation.resume(throwing: error) }
+            let result = data.flatMap {try? $0.toDictionary()}
+            if let json = result?["result"] {
+                continuation.resume(returning: json)
+            } else if let httpResponse = response as? HTTPURLResponse {
+                let json: Data = (try? JSONSerialization.data(withJSONObject: result, options: [.fragmentsAllowed])) ?? .init()
+                debugPrint("=====================")
+                print(String(decoding: json, as: UTF8.self))
+                debugPrint("=====================")
+                let error = HTTPError.error(status: httpResponse.statusCode,
+                                            message: data.flatMap({ String(data: $0, encoding: .utf8) }))
+                continuation.resume(throwing: error)
+            } else {
+                continuation.resume(throwing: HTTPError.unknown)
+            }
+        }
+        task.resume()
+    })
 }
 
 public func fetchJson(connection: ConnectionInfo, json: [String: Any]?) async throws -> Any {
-  let url = connection.url
-  return try await fetch(url: url, params: json)
+    let url = connection.url
+    return try await fetch(url: url, params: json)
 }
 
 func fetchJson(url: URL, json: [String: Any]?) async throws -> Any {
-  return try await fetch(url: url, params: json)
+    return try await fetch(url: url, params: json)
 }
